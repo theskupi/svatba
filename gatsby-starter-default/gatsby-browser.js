b6638bca00d62a3107b9bdc53c578f7267d0aaf5
Vlastimil Skupien <vlastikskupien@seznam.cz>
2022-01-17 21:57:46 +0100

    finally working

--- gatsby-starter-default/gatsby-browser.js
+++ gatsby-starter-default/gatsby-browser.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+// import React from "react"
+// import { ParallaxProvider } from "react-scroll-parallax"
+
+// export const WrapPageElement = ({ children }) => {
+//   return <ParallaxProvider>{children}</ParallaxProvider>
+// }
