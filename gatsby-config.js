20c9e1b2b2b7064a6cd05fad75bebb7da6f8e338
Vlastimil Skupien <vlastikskupien@seznam.cz>
2022-02-10 13:13:09 +0100

    fix: show env variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+console.log(process.env.AIRTABLE_API)
+
 module.exports = {
   siteMetadata: {
     title: `Dáša & Vlastík`,
